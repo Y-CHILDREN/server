9f2c855c4d2d21a4d371f1b2368b2aa3ee8c0def
Layton <jihwan8807@naver.com>
2024-09-29 19:19:34 +0900

    node, typescript 초기 세팅

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { Request } from 'express';
+import cors from 'cors';
+
+const app = express();
+
+dotenv.config();
+
+app.set('port', process.env.PORT);
+
+app.use(cors<Request>());
+app.listen(app.get('port'), async () => {
+  console.log('hi');
+});
