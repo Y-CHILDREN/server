61de091ca1a1441b055521c7f7bff8756bf4b931
Yungoo Kong <ohsory1324@gmail.com>
2024-10-05 15:55:21 +0900

    test: fix date issue

--- src/tests/domain/services/calculationService.test.ts
+++ src/tests/domain/services/calculationService.test.ts
@@ -2,6 +2,10 @@ import { CalculationData } from '../../../domain/entities/calculationData';
 import { CalculationService } from '../../../domain/services/calculationService';
 
 describe('Test CalculationService', () => {
+  beforeEach(() => {
+    vi.clearAllTimers();
+  });
+
   describe('Test CalculationService.getNumber', () => {
     test('repository에서 주는 number를 그대로 반환해야한다.', async () => {
       // Given
@@ -25,6 +29,7 @@ describe('Test CalculationService', () => {
   describe('Test CalculationService.plusOne', () => {
     test('repository에 있는 number에 1을 더한 값을 repository에 저장하고, 그 값을 반환해야한다.', async () => {
       // Given
+      vi.useFakeTimers();
       const expected = {
         number: 2,
         updatedAt: new Date(),
@@ -51,6 +56,7 @@ describe('Test CalculationService', () => {
   describe('Test CalculationService.minusOne', () => {
     test('repository에 있는 number에 1을 뺀 값을 repository에 저장하고, 그 값을 반환해야한다.', async () => {
       // Given
+      vi.useFakeTimers();
       const expected = {
         number: 0,
         updatedAt: new Date(),
