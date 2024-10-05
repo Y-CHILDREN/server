036c450b325e22bc7e6eb851309a9b90967d9538
Taejeong <mkiz0403@gmail.com>
2024-10-05 09:34:28 +0900

    add : 구글, 유저 라우터 import

--- index.ts
+++ index.ts
@@ -2,14 +2,31 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { Request } from 'express';
 import cors from 'cors';
+import passport from 'passport';
+import googleRouter from './routes/googleRouter';
+import userRouter from './routes/userRouter';
+import configurePassprot from './config/auth/googleOAuth';
 
 const app = express();
 
 dotenv.config();
 
-app.set('port', process.env.PORT);
+// app.set('port', process.env.PORT);
+const PORT = 3000;
 
 app.use(cors<Request>());
-app.listen(app.get('port'), async () => {
-  console.log('Hello, world!');
+
+app.use(passport.initialize());
+
+configurePassprot(passport);
+
+app.use('/', googleRouter);
+
+app.use('/', userRouter);
+
+// app.listen(app.get('port'), async () => {
+//   console.log(`Hello, world!`);
+// });
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
