7646ed8472b1b1eefa3be5bd784edd87592a6972
Taejeong <mkiz0403@gmail.com>
2024-10-08 18:47:47 +0900

    feat : naver 인증 추가

---
+++ src/domain/entities/auth/naverOAuth.ts
@@ -0,0 +1,65 @@
+import passport from 'passport';
+import {
+  Strategy as NaverStrategy,
+  Profile as NaverProfile,
+} from 'passport-naver-v2';
+
+import {
+  createUser,
+  findUserByEmail,
+  updateTokens,
+  findUserById,
+} from '../userModel';
+
+const configuresNaverPassport = (passport: any) => {
+  passport.use(
+    new NaverStrategy(
+      {
+        clientID: process.env.NAVER_CLIENT_ID,
+        clientSecret: process.env.NAVER_CLIENT_SECRET,
+        callbackURL: process.env.NAVER_REDIRECT_URI,
+      },
+      async (
+        accessToken: string,
+        refreshToken: string,
+        profile: NaverProfile,
+        done: any
+      ) => {
+        console.log(profile);
+        try {
+          const data = profile._json;
+          let user = findUserById(profile.id || '');
+
+          if (!user) {
+            const newUser = createUser({
+              email: profile.email || '',
+              user_image: profile.profileImage || '',
+              profile: profile.name || '',
+              accessToken: accessToken,
+              refreshToken: refreshToken,
+            });
+            return done(null, newUser);
+          }
+          updateTokens(profile.email || '', accessToken, refreshToken);
+          return done(null, user);
+        } catch (error) {
+          return done(error, false);
+        }
+      }
+    )
+  );
+  passport.serializeUser((user: any, done: any) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id: string, done: any) => {
+    const user = findUserById(id);
+    if (user) {
+      done(null, user);
+    } else {
+      done(new Error('유저를 찾을수 없습니다'), null);
+    }
+  });
+};
+
+export default configuresNaverPassport;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import express from 'express';
-
 import dotenv from 'dotenv';
 import { Request } from 'express';
 import cors from 'cors';
+
 import passport from 'passport';
-import googleRouter from './presentation/routes/googleRouter';
-import userRouter from './presentation/routes/userRouter';
-import configurePassprot from './domain/entities/auth/googleOAuth';
 import session from 'express-session';
+
+import googleRouter from './presentation/routes/googleRouter';
+import naverRouter from './presentation/routes/naverRouter';
+import userRouter from './presentation/routes/userRouter';
+
+import configureGooglePassport from './domain/entities/auth/googleOAuth';
+import configuresNaverPassport from './domain/entities/auth/naverOAuth';
 
 const app = express();
 
@@ -28,10 +32,11 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-configurePassprot(passport);
+configureGooglePassport(passport);
+configuresNaverPassport(passport);
 
 app.use('/', googleRouter);
-
+app.use('/', naverRouter);
 app.use('/', userRouter);
 
 app.listen(app.get('port'), async () => {
