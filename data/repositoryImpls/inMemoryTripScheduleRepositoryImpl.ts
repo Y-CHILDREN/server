d7416ca0d89a19d828d9205c970a6026690f9276
Wannyon <ghkdwodnjs123@naver.com>
2024-10-11 05:34:28 +0900

    in-memory trip schedule repo-Impl

---
+++ data/repositoryImpls/inMemoryTripScheduleRepositoryImpl.ts
@@ -0,0 +1,23 @@
+import {TripScheduleRepository} from "../../domain/repositories/tripScheduleRepository";
+import {TripScheduleData} from "../../domain/entities/tripScheduleData";
+
+const trips: TripScheduleData[] = [];
+export class InMemoryTripScheduleRepositoryImpl implements TripScheduleRepository {
+    async create(trip: TripScheduleData): Promise<TripScheduleData> {
+        trips.push(trip);
+        return trip;
+    }
+
+    async update(trip:TripScheduleData): Promise<void> {
+        const index = trips.findIndex(t => t.id === trip.id);
+
+        // find Index
+        if (index !== -1) {
+            trips[index] = trip;
+        }
+    }
+
+    async findTripById(id:number): Promise<TripScheduleData | null> {
+        return trips.find(trip => trip.id === id) || null;
+    }
+}
