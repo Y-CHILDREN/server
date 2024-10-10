31ebe3364589721ff1e5f2ef2f785abf52daa51c
Taejeong <mkiz0403@gmail.com>
2024-10-10 12:05:45 +0900

    modify : repositories로 경로이동

---
+++ src/domain/repositories/userModel.ts
@@ -0,0 +1,43 @@
+interface User {
+  id: string;
+  email: string;
+  user_image: string;
+  profile: string;
+  accessToken: string;
+  refreshToken: string;
+}
+
+let users: User[] = [];
+
+export const createUser = (userData: Omit<User, 'id'>): User => {
+  const newUser: User = {
+    id: String(users.length + 1),
+    ...userData,
+  };
+  users.push(newUser);
+  return newUser;
+};
+
+export const findUserByEmail = (email: string): User | undefined => {
+  return users.find((user) => user.email === email);
+};
+
+export const findUserById = (id: string): User | undefined => {
+  return users.find((user) => user.id === id);
+};
+
+export const updateTokens = (
+  email: string,
+  accessToken: string,
+  refreshToken: string
+) => {
+  const user = findUserByEmail(email);
+  if (user) {
+    user.accessToken = accessToken;
+    user.refreshToken = refreshToken;
+  }
+};
+
+export const getAllUser = (): User[] => {
+  return users;
+};
