b12b160e14f1e6a8bb674f14221a947b9ac1e814
Taejeong <mkiz0403@gmail.com>
2024-10-11 18:07:24 +0900

    feat : 구글 로그아웃 코드 추가

--- src/presentation/routes/googleRouter.ts
+++ src/presentation/routes/googleRouter.ts
@@ -1,8 +1,10 @@
+import { rejects } from 'assert';
 import { Router } from 'express';
 import passport from 'passport';
 
 const googleRouter = Router();
 
+// 로그인 라우터
 googleRouter.get(
   '/auth/google',
   passport.authenticate('google', { scope: ['profile', 'email'] })
@@ -15,5 +17,22 @@ googleRouter.get(
     res.send('구글 인증 성공');
   }
 );
+
+// 구글 로그아웃 
+googleRouter.get('/auth/google/logout', (req, res, next) => {
+  req.logOut((err) => {
+    if (err) {
+      return next(err);
+    }
+
+    req.session.destroy((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.clearCookie('connect.sid');
+      res.redirect('/');
+    });
+  });
+});
 
 export default googleRouter;
