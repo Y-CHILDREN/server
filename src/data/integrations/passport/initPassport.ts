e065e98dd74dd5a105cb4d2141e20f9796f4dc1a
Taejeong <mkiz0403@gmail.com>
2024-10-11 21:26:20 +0900

    del : passport.initialize()   passport.session()  코드 삭제

--- src/data/integrations/passport/initPassport.ts
+++ src/data/integrations/passport/initPassport.ts
@@ -1,12 +1,10 @@
+import express from 'express';
 import passport from 'passport';
 import configureGooglePassport from './googleOAuth';
 import configuresNaverPassport from './naverOAuth';
 import configuresKakaoPassport from './kakoOAuth';
 
 function initPassport() {
-  passport.initialize();
-  passport.session();
-
   configureGooglePassport(passport);
   configuresNaverPassport(passport);
   configuresKakaoPassport(passport);
