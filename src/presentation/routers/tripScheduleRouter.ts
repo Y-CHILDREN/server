0532e9ae8765a4d4d9f78ceb8acc0b69b33df431
Wannyon <ghkdwodnjs123@naver.com>
2024-10-17 19:02:20 +0900

    feat: tripSchedule Router.

---
+++ src/presentation/routers/tripScheduleRouter.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import { TripScheduleController } from '../controllers/tripScheduleController';
+
+const router = express.Router();
+
+const tripScheduleController = new TripScheduleController();
+
+// Create Trip
+router.post('/trips', (req, res) =>
+  tripScheduleController.createTrip(req, res),
+);
+
+// Add member to trip
+router.post('/trips/members', (req, res) =>
+  tripScheduleController.addMemberByEmail(req, res),
+);
+
+// Get trip by id
+router.get('/trips/:id', (req, res) =>
+    void tripScheduleController.getTripById(req, res)
+);
+
+export default router;
