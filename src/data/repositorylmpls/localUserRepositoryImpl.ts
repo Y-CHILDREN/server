00d890346b68ee382ce5c0d8e3d780cee823033b
Taejeong <mkiz0403@gmail.com>
2024-10-19 11:29:35 +0900

    feat : 주석 입력

--- src/data/repositorylmpls/localUserRepositoryImpl.ts
+++ src/data/repositorylmpls/localUserRepositoryImpl.ts
@@ -6,6 +6,7 @@ import { UserRepository } from '../../domain/models/userRepository';
 export const userDataLocalRepository = (): UserRepository => {
   const filePath = path.join(__dirname, './userData.json');
 
+  // userData.json 파일을 읽는 함수
   const readUsersData = (): Promise<User[]> => {
     return new Promise((resolve, reject) => {
       fs.readFile(filePath, 'utf-8', (err, data) => {
@@ -22,6 +23,7 @@ export const userDataLocalRepository = (): UserRepository => {
     });
   };
 
+  // userData.json에 데이터를 입력하는 함수
   const writeUsersData = (users: User[]): Promise<void> => {
     return new Promise((resolve, reject) => {
       fs.writeFile(filePath, JSON.stringify(users, null, 2), (err) => {
@@ -35,6 +37,7 @@ export const userDataLocalRepository = (): UserRepository => {
     });
   };
 
+  // 유저 생성함수
   const createUser = async (userData: Omit<User, 'id'>): Promise<User> => {
     const users = await readUsersData();
     const newUser: User = {
@@ -47,11 +50,13 @@ export const userDataLocalRepository = (): UserRepository => {
     return newUser;
   };
 
+  // 유저 이메일로 유저를 체크하는 함수
   const findUserByEmail = async (email: string): Promise<User | undefined> => {
     const users = await readUsersData();
     return users.find((user) => user.email === email);
   };
 
+  // 유저 이메일과 provider(유저 가입 플랫품)로 유저를 체크하는 함수
   const findUserByEmailAndProvider = async (
     email: string,
     provider: string
@@ -62,24 +67,28 @@ export const userDataLocalRepository = (): UserRepository => {
     );
   };
 
+  // id로 유저를 체크하는 함수
   const findUserById = async (id: string): Promise<User | undefined> => {
     const users = await readUsersData();
     return users.find((user) => user.id === id);
   };
 
+  // 유저 이미지를 수정하는 함수
   const updateUserImage = async (id: string, user_image: string) => {
     const users = await readUsersData();
     const user = users.find((user) => user.id === id);
     if (user) {
       user.user_image = user_image;
       await writeUsersData(users);
-      console.log('유저 메모를 수정했습니다.');
+      console.log('유저 이미지를 수정했습니다.');
       return user;
     } else {
-      console.log('유저 메모 업데이트에 실패했습니다.');
+      console.log('유저 이미지 업데이트에 실패했습니다.');
       return undefined;
     }
   };
+
+  // 유저 닉네임을 수정하는 함수
   const updateUserNickname = async (id: string, nickname: string) => {
     const users = await readUsersData();
     const user = users.find((user) => user.id === id);
@@ -92,6 +101,8 @@ export const userDataLocalRepository = (): UserRepository => {
     console.log('유저 닉네임 업데이트에 실패했습니다.');
     return undefined;
   };
+
+  //유저 메모를 수정한느 함수
   const updateUserMemo = async (id: string, user_memo: string) => {
     const users = await readUsersData();
     const user = users.find((user) => user.id === id);
@@ -106,6 +117,7 @@ export const userDataLocalRepository = (): UserRepository => {
     }
   };
 
+  //로그인시 유저 토큰 생성하는 함수
   const updateTokens = async (
     email: string,
     access_token: string,
@@ -120,6 +132,7 @@ export const userDataLocalRepository = (): UserRepository => {
     }
   };
 
+  // 유저를 호출하는 함수
   const getAllUsers = async (): Promise<User[]> => {
     return await readUsersData();
   };
