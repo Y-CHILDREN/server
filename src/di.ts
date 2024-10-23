370c9b58f13c57f1b3b29cd20cc3edd4122c09f8
Taejeong <mkiz0403@gmail.com>
2024-10-23 14:19:59 +0900

    feat : 유저 레포, 유저 서비스 추가

--- src/di.ts
+++ src/di.ts
@@ -1,9 +1,17 @@
 import express from 'express';
 import { CalculationService } from './domain/services/calculationService';
 import { InMemoryCalculationRepositoryImpl } from './data/repositoryImpls/inMemoryCalculationRepositoryImpl';
+import { userDataLocalRepository } from './data/repositoryImpls/localUserRepositoryImpl';
+import { UserService } from './domain/services/userService';
 
 export function di(app: ReturnType<typeof express>) {
   const calculationRepository = new InMemoryCalculationRepositoryImpl();
   const calculationService = new CalculationService(calculationRepository);
   app.set('calculationService', calculationService);
+
+  // User
+  const userRepository = userDataLocalRepository();
+  const userService = UserService(userRepository);
+  app.set('userService', userService);
+
 }
