066562393d8c2f454dee5cac8b79981f29858473
Taejeong <mkiz0403@gmail.com>
2024-10-23 14:13:32 +0900

    fix : 컨트롤러가 UserService랑 연결 되도록 수정

--- src/presentation/controllers/userController.ts
+++ src/presentation/controllers/userController.ts
@@ -1,73 +1,76 @@
 import { Request, Response } from 'express';
-import { userDataLocalRepository } from '../../data/repositoryImpls/localUserRepositoryImpl';
-
-const userRepository = userDataLocalRepository();
+import {UserService} from '../../domain/services/userService';
 
 export const getUserById = async (
-  req: Request<{ id: string }>,
-  res: Response,
-) => {
-  const { id } = req.params;
+    req: Request<{ id: string }>,
+    res: Response,
+  ) => {
+    const userService = req.app.get('userService') as ReturnType<typeof UserService>;
+    const { id } = req.params;
 
-  try {
-    const user = await userRepository.findUserById(id);
-    if (user) {
-      res.json(user);
-    } else {
-      res.status(404).json({ message: '유저를 찾을 수 없습니다.' });
+  
+    try {
+      const user = await userService.findUserById(id);
+      if (user) {
+        res.json(user);
+      } else {
+        res.status(404).json({ message: '유저를 찾을 수 없습니다.' });
+      }
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: '유저 정보를 가져오는 중 오류가 발생했습니다.' });
     }
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: '유저 정보를 가져오는 중 오류가 발생했습니다.' });
-  }
-};
-
+  };
+  
 export const updateUserNickname = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const { nickname } = req.body;
-
-  if (!nickname) {
-    return res.status(400).json({ message: '닉네임을 입력해주세요' });
-  }
-
-  try {
-    const updatedUserNickname = await userRepository.updateUserNickname(
-      id,
-      nickname,
-    );
-
-    if (updatedUserNickname) {
-      res.json({
-        message: '유저 닉네임이 업데이트 되었습니다.',
-        user: updatedUserNickname,
-      });
-    } else {
-      res.status(404).json({ message: '유저를 찾을 수 없습니다.' });
+    const userService = req.app.get('userService') as ReturnType<typeof UserService>;
+    const { id } = req.params;
+    const { nickname } = req.body;
+  
+    if (!nickname) {
+      return res.status(400).json({ message: '닉네임을 입력해주세요' });
     }
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: '닉네임 업데이트 중 오류가 발생했습니다.' });
-  }
-};
-
+  
+    try {
+      const updatedUserNickname = await userService.updateUserNickname(
+        id,
+        nickname,
+      );
+  
+      if (updatedUserNickname) {
+        res.json({
+          message: '유저 닉네임이 업데이트 되었습니다.',
+          user: updatedUserNickname,
+        });
+      } else {
+        res.status(404).json({ message: '유저를 찾을 수 없습니다.' });
+      }
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: '닉네임 업데이트 중 오류가 발생했습니다.' });
+    }
+  };
+  
 export const updateUserMemo = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const { user_memo } = req.body;
-
-  try {
-    const updatedUserMemo = await userRepository.updateUserMemo(id, user_memo);
-
-    if (updatedUserMemo) {
-      res.json({
-        message: '유저 메모가 업데이트 되었습니다.',
-        user: updatedUserMemo,
-      });
-    } else {
-      res.status(404).json({ message: '유저를 찾을 수 없습니다.' });
+    const userService = req.app.get('userService') as ReturnType<typeof UserService>;
+    const { id } = req.params;
+    const { user_memo } = req.body;
+  
+    try {
+      const updatedUserMemo = await userService.updateUserMemo(id, user_memo);
+  
+      if (updatedUserMemo) {
+        res.json({
+          message: '유저 메모가 업데이트 되었습니다.',
+          user: updatedUserMemo,
+        });
+      } else {
+        res.status(404).json({ message: '유저를 찾을 수 없습니다.' });
+      }
+    } catch (error) {
+      res.status(500).json({ message: '메모 업데이트 중 오류가 발생했습니다.' });
     }
-  } catch (error) {
-    res.status(500).json({ message: '메모 업데이트 중 오류가 발생했습니다.' });
-  }
-};
+  };
+
