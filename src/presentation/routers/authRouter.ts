da3c1ec97bbae47a8901ea5061b0212a49ea5d56
Taejeong <mkiz0403@gmail.com>
2024-11-06 18:58:16 +0900

    fix : 사용하지 않는 라우터 코드 삭제

--- src/presentation/routers/authRouter.ts
+++ src/presentation/routers/authRouter.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { UserService } from '../../domain/services/userService';
+
 import googleRouter from './auth/googleRouter';
 import naverRouter from './auth/naverRouter';
 import kakaoRouter from './auth/kakaoRouter';
@@ -10,29 +12,5 @@ authRouter.use('/google', googleRouter);
 authRouter.use('/naver', naverRouter);
 authRouter.use('/kakao', kakaoRouter);
 authRouter.use('/users', userRouter);
-
-authRouter.get('/logout', (req, res, next) => {
-  req.logout((err) => {
-    if (err) {
-      return next(err);
-    }
-
-    req.session.destroy((err) => {
-      if (err) {
-        return next(err);
-      }
-      res.clearCookie('connect.sid');
-      res.redirect('/');
-    });
-  });
-});
-
-authRouter.get('/profile', (req, res) => {
-  if (req.isAuthenticated()) {
-    res.json({ message: '로그인된 유저가 있습니다.', user: req.user });
-  } else {
-    res.status(401).json({ message: '로그인된 유저가 없습니다.' });
-  }
-});
 
 export default authRouter;
