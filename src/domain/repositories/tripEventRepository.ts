ac3a04198287edc53e315eae552fe4a9461c1915
Layton <jihwan8807@naver.com>
2024-11-15 00:51:01 +0900

    refactor : tripEventReopository 타입명 적절하게 변경

--- src/domain/repositories/tripEventRepository.ts
+++ src/domain/repositories/tripEventRepository.ts
@@ -1,9 +1,11 @@
 import { TripEvent } from '../entities/tripEvent.js';
 
 export interface TripEventRepository {
-  create(tripSchedule: Omit<TripEvent, 'id'>): Promise<TripEvent>;
-  update(tripSchedule: TripEvent): Promise<TripEvent>;
-  deleteById(id: number): Promise<boolean>;
-  findTripEventById(id: number): Promise<TripEvent | null>; // event id로 단일 조회
-  findTripEventsByTripId(tripId: number[]): Promise<TripEvent[]>; // 여행 id로 여러 event로 조회
+  createTripEvent(
+    tripEventData: Omit<TripEvent, 'event_id'>,
+  ): Promise<TripEvent>;
+  updateTripEvent(tripEventData: TripEvent): Promise<TripEvent>;
+  deleteTripEventById(event_id: number): Promise<boolean>;
+  getTripEventById(event_id: number): Promise<TripEvent | null>; // event id로 단일 조회
+  getTripEventsByTripId(trip_id: number): Promise<TripEvent[] | null>; // 여행 id로 여러 event로 조회
 }
